b0f2d5b3fdc7e8665ef3920bcbcdc8348e5d758d
ginter <scony8877@gmail.com>
2024-05-13 15:17:46 +0800

    create an array called stuff

--- PracticeQuestions.ts
+++ PracticeQuestions.ts
@@ -1,2 +1,8 @@
 //1.Create a variable called highScore that can be a number OR a boolean
 let hightScore: number | boolean;
+
+
+//2.create an array called stuff
+//it can be an array of numbers OR an array of strings
+//it cannot be an array of numbers and strings (mixed together)
+let stuff: number[] | string[];
