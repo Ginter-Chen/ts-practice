639fb4f9cdc0554e379a5219951523c391e2a02b
ginter <scony8877@gmail.com>
2024-05-13 17:19:47 +0800

    Write a function called greet that accepts a single string OR an array of strings

--- PracticeQuestions.ts
+++ PracticeQuestions.ts
@@ -6,3 +6,15 @@ let hightScore: number | boolean;
 //it can be an array of numbers OR an array of strings
 //it cannot be an array of numbers and strings (mixed together)
 let stuff: number[] | string[];
+
+//3.Write a function called greet that accepts a single string OR an array of strings
+//It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+const greet = (name: string | string[]) => {
+    if (typeof name === "string"){
+        console.log(`Hello,${name}`);
+    }else{
+        name.forEach(element => {
+            console.log(`Hello,${element}`);
+        });
+    }
+}
