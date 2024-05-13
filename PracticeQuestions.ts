a27b00a7f22aab5da6fd33b89400b062efa3bb18
ginter <scony8877@gmail.com>
2024-05-13 17:21:30 +0800

    Given this interface

--- PracticeQuestions.ts
+++ PracticeQuestions.ts
@@ -10,11 +10,21 @@ let stuff: number[] | string[];
 //3.Write a function called greet that accepts a single string OR an array of strings
 //It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
 const greet = (name: string | string[]) => {
-    if (typeof name === "string"){
+    if (typeof name === "string") {
         console.log(`Hello,${name}`);
-    }else{
+    } else {
         name.forEach(element => {
             console.log(`Hello,${element}`);
         });
+    }
+}
+
+//4.Given this interface: Select the answer below that is a valid implementation of the Cryable interface
+interface Cryable {
+    cry(): string;
+}
+const human: Cryable = {
+    cry() {
+        return "BOO HOO"
     }
 }
